2b9ebd9656b1cec1d8667762fdf29843095c75f9
coDex99 <abednegodunyah@gmail.com>
2025-05-02 00:36:07 +0100

    Set up PostgreSQL and added initial models and migrations

--- btre_project/settings.py
+++ btre_project/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = "btre_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "btredb",
+        "USER": "postgres",
+        "HOST": "localhost",
+        "PASSWORD": "postgres",
+        "PORT": "5432",
     }
 }
 
